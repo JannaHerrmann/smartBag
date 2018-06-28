324c319366397845993f4e71f3a54904b2c34bac
jahe01 <>
2018-06-28 15:07:28 +0200

    [Janna] Center current user location in map

--- SmartBag/SmartBag/TheftViewController.swift
+++ SmartBag/SmartBag/TheftViewController.swift
@@ -8,7 +8,69 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class TheftViewController : UIViewController{
-	
+
+class TheftViewController : UIViewController, CLLocationManagerDelegate{
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager : CLLocationManager!
+    var lastLocation : CLLocation!
+    var isCentered = false
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.centerView()
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        determineMyCurrentLocation()
+    }
+    
+    func centerView(){
+        self.mapView.setCenter(lastLocation.coordinate, animated: true)
+        self.mapView.setRegion(MKCoordinateRegionMake(lastLocation.coordinate, MKCoordinateSpan.init(latitudeDelta: 0.0, longitudeDelta: 0.0)), animated: true)
+    }
+    
+    func determineMyCurrentLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+            //locationManager.startUpdatingHeading()
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        
+        // Call stopUpdatingLocation() to stop listening for location updates,
+        // other wise this function will be called every time when user location changes.
+        
+        // manager.stopUpdatingLocation()
+        
+        lastLocation = userLocation
+        
+        //center once at the start
+        if (!isCentered){
+            isCentered = true
+            self.centerView()
+        }
+
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        print("Error \(error)")
+    }
 }
