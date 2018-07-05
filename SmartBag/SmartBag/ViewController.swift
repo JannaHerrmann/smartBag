1376329a1a2eb4b78fb3b56e8e10e2f2d8f23d69
Janna Herrmann <>
2018-07-05 11:05:27 +0200

    Buttons

--- SmartBag/SmartBag/ViewController.swift
+++ SmartBag/SmartBag/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	@IBOutlet var topView: UIView!
+    @IBOutlet weak var theftButton: UIButton!
+    @IBOutlet var topView: UIView!
+    @IBOutlet weak var lostButton: UIButton!
+    
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		self.topView.layer.borderWidth = 1.0
 		self.topView.layer.borderColor = #colorLiteral(red: 0.2056839466, green: 0.4766893387, blue: 0.4690987468, alpha: 1)
+        
+        self.theftButton.layer.borderWidth = 1.0
+        self.theftButton.layer.borderColor = #colorLiteral(red: 0.2056839466, green: 0.4766893387, blue: 0.4690987468, alpha: 1)
+        self.theftButton.layer.cornerRadius = 10
+        self.theftButton.layer.borderWidth = 1
+        
+        
+        
+        self.lostButton.layer.borderWidth = 1.0
+        self.lostButton.layer.borderColor = #colorLiteral(red: 0.2056839466, green: 0.4766893387, blue: 0.4690987468, alpha: 1)
+        self.lostButton.layer.cornerRadius = 10
+        self.lostButton.layer.borderWidth = 1
+
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
